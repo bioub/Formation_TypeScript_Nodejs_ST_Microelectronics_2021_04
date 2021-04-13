e556d019b6639408659ff586ea91a3472dc695d6
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-04-13 16:43:44 +0200

    Correction Objet

---
+++ javascript/ex-jeu-object.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const Random = {
+  get: function () {
+    return Math.random();
+  },
+  getArbitrary: function (min, max) {
+    return Math.random() * (max - min) + min;
+  },
+  getInt: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  },
+  getIntInclusive: function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  },
+};
+
+// import de l'API readline de Node.js
+const readline = require('readline');
+
+function Jeu(options = {}) {
+  const min = options.min ?? 0;
+  const max = options.max ?? 100;
+  this.rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  this.entierAlea = Random.getInt(min, max);
+  this.essais = [];
+}
+
+Jeu.prototype.jouer = function () {
+  if (this.essais.length) {
+    console.log('Vous avez déjà joué : ' + this.essais.join(' - '));
+  }
+
+  this.rl.question('Quel est le nombre ? ', (answer) => {
+
+    const entierSaisi = Number.parseInt(answer, 10);
+
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Il faut saisir un nombre');
+      return this.jouer();
+    }
+
+    this.essais.push(entierSaisi);
+
+    if (entierSaisi < this.entierAlea) {
+      console.log('Trop petit');
+      this.jouer();
+    } else if (entierSaisi > this.entierAlea) {
+      console.log('Trop grand');
+      this.jouer();
+    } else {
+      console.log('Gagné');
+      this.rl.close();
+    }
+  });
+}
+
+const game = new Jeu();
+game.jouer();
