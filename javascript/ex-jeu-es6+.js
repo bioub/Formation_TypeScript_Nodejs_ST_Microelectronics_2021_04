f5a4a0e9530998aceca4b7ae98893f49b97fee6f
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-04-14 13:17:55 +0200

    Correction ES6

--- javascript/ex-jeu-es6+.js
+++ javascript/ex-jeu-es6+.js
@@ -2,18 +2,18 @@
 
 // 1 - Method properties
 const Random = {
-  get: function () {
+  get() {
     return Math.random();
   },
-  getArbitrary: function (min, max) {
+  getArbitrary(min, max) {
     return Math.random() * (max - min) + min;
   },
-  getInt: function (min, max) {
+  getInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   },
-  getIntInclusive: function (min, max) {
+  getIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
@@ -24,51 +24,58 @@ const Random = {
 const readline = require('readline');
 
 // 2 - Class
-function Jeu(options = {}) {
-
-  // 3 - Object Destructuring + Default Param
-  const min = options.min ?? 0;
-  // const min = options.min !== undefined ? options.min : 0;
-  const max = options.max ?? 100;
-  this._rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  this.entierAlea = Random.getInt(min, max);
-  this.essais = [];
-}
-
-Jeu.prototype.jouer = function () {
-  if (this.essais.length) {
-    // 4 - Template literal
-    console.log('Vous avez déjà joué : ' + this.essais.join(' - '));
+class Jeu {
+  /**
+   * @param {object} options
+   * @param {number} options.min
+   * @param {number} options.max
+   */
+  constructor(options = {}) {
+    // 3 - Object Destructuring + Default Param
+    const { min = 0, max = 100 } = options;
+    this._rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.entierAlea = Random.getInt(min, max);
+    this.essais = [];
   }
-
-  this._rl.question('Quel est le nombre ? ', (answer) => {
-
-    const entierSaisi = Number.parseInt(answer, 10);
-
-    if (Number.isNaN(entierSaisi)) {
-      console.log('Il faut saisir un nombre');
-      return this.jouer();
+  jouer() {
+    if (this.essais.length) {
+      // 4 - Template literal
+      console.log(`Vous avez déjà joué : ${this.essais.join(' - ')} !!!`);
     }
 
-    this.essais.push(entierSaisi);
+    this._rl.question('Quel est le nombre ? ', (answer) => {
 
-    if (entierSaisi < this.entierAlea) {
-      console.log('Trop petit');
-      this.jouer();
-    } else if (entierSaisi > this.entierAlea) {
-      console.log('Trop grand');
-      this.jouer();
-    } else {
-      console.log('Gagné');
-      this._rl.close();
-    }
-  });
+      const entierSaisi = Number.parseInt(answer, 10);
+
+      if (Number.isNaN(entierSaisi)) {
+        console.log('Il faut saisir un nombre');
+        return this.jouer();
+      }
+
+      this.essais.push(entierSaisi);
+
+      if (entierSaisi < this.entierAlea) {
+        console.log('Trop petit');
+        this.jouer();
+      } else if (entierSaisi > this.entierAlea) {
+        console.log('Trop grand');
+        this.jouer();
+      } else {
+        console.log('Gagné');
+        this._rl.close();
+      }
+    });
+  }
 }
 
-const game = new Jeu();
+
+const game = new Jeu({
+  min: 50,
+  max: 60,
+});
 game.jouer();
 
 
